43e423a5904f40043c89c9d0d822a774e3a7312b
ahmaadaadel <ahmaadaadel@yahoo.co.uk>
2020-02-15 08:54:47 +0200

    Creating ORACLE Sequences

---
+++ DDL/002_15_Feb_2020_Sabagh/01_sequences.sql
@@ -0,0 +1,39 @@
+      CREATE SEQUENCE area_sequence
+      INCREMENT BY 1
+      START WITH 1000;
+      
+      CREATE SEQUENCE box_sequence
+      INCREMENT BY 1
+      START WITH 1000;
+      
+      CREATE SEQUENCE cabin_sequence
+      INCREMENT BY 1
+      START WITH 1000;
+      
+      CREATE SEQUENCE city_sequence
+      INCREMENT BY 1
+      START WITH 1000;
+      
+      CREATE SEQUENCE country_sequence
+      INCREMENT BY 1
+      START WITH 1000;
+      
+      CREATE SEQUENCE feature_sequence
+      INCREMENT BY 1
+      START WITH 1000;
+      
+      CREATE SEQUENCE landline_phone_sequence
+      INCREMENT BY 1
+      START WITH 1000;
+      
+      CREATE SEQUENCE logistict_sequence
+      INCREMENT BY 1
+      START WITH 1000;
+      
+      CREATE SEQUENCE phone_user_sequence
+      INCREMENT BY 1
+      START WITH 1000;
+      
+      CREATE SEQUENCE user_feaure_sequence
+      INCREMENT BY 1
+      START WITH 1000;
